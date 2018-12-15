89b11ab5e8f9a3fb536e5fd52e03f0fd2eec2143
Low Jun Chen Marx <marxlow94@gmail.com>
2018-12-15 18:40:32 +0800

    set up webpack and babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import 'babel-polyfill';
-
+import '@babel/polyfill';
 import express from 'express';
 
 if (process.env.NODE_ENV === 'local') {
@@ -9,18 +8,16 @@ if (process.env.NODE_ENV === 'local') {
 let app = express();
 
 /* Set up apps with routers and their root URLs here */
-// app.use
 
 // Starting up the server
-console.log('> Port is:', process.env.PORT);
 app.listen(process.env.PORT, async (error) => {
   if (error) {
     console.log(`Error initializing server: ${error.message}`);
   }
   if (process.env.NODE_ENV === 'local') {
-    console.log(`> Server is ready on http://localhost:${process.env.PORT}`);
+    console.log(`> Server is ready on "http://localhost:${process.env.PORT}"`);
   } else {
-    console.log('> Server is ready!');
+    console.log('> Server is ready & is running on production!');
   }
 })
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js'],
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [{
+      test: /(\.js)$/,
+      exclude: /(node_modules|dist)/,
+      use: [{
+        loader: 'babel-loader'
+      }]
+    }]
+  },
+  node: {
+    fs: "empty",
+    net: "empty"
+  },
+  mode: 'production',
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
 };
